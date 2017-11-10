82cc1f68ae62dd397a318b5bcb491aab6fbf2dff
Administrador <Administrador@LABIN2-PROF>
2017-11-10 10:42:58 -0200

    adaptações

--- ConexaoBanco/src/conexaobanco/controller/ClienteController.java
+++ ConexaoBanco/src/conexaobanco/controller/ClienteController.java
@@ -5,7 +5,7 @@
  */
 package conexaobanco.controller;
 
-import conexaobanco.Conexao;
+import persistencia.Conexao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
