2f26412c17199d6cbfad61fd5dd23f99f74c1610
Administrador <Administrador@LABIN2-PROF>
2017-11-10 12:11:56 -0200

    Data e Hora

--- ConexaoBanco/src/dao/FilmeDao.java
+++ ConexaoBanco/src/dao/FilmeDao.java
@@ -5,10 +5,12 @@
  */
 package dao;
 
+import java.sql.Date;
 import persistencia.Conexao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -17,35 +19,34 @@ import javax.swing.JOptionPane;
  *
  * @author Administrador
  */
-public class PessoaDao {
+public class FilmeDao {
 
-    public boolean inserir(String nome, int cpf) {
-        String sql = "INSERT INTO pessoa(nome,cpf) VALUES (?, ?)";//define instrução SQL
+    public boolean inserir(String titulo, String duracao, String dataEstreia, String genero) {
+        String sql = "INSERT INTO filme(titulo, duracao, data_estreia, genero) VALUES (?, ?, ?, ?)";//define instrução SQL
         PreparedStatement ps;
         try {
             ps = Conexao.getConexao().prepareStatement(sql);//prepara instrução SQL
-            ps.setString(1, nome);// primeiro parâmetro indica a ? correspondente, segundo parâmetro a variável que substituirá a ?
-            ps.setInt(2, cpf); // primeiro parâmetro indica a ? correspondente, segundo parâmetro a variável que substituirá a ?
+            ps.setString(1, titulo);// primeiro parâmetro indica a ? correspondente, segundo parâmetro a variável que substituirá a ?
+            ps.setTime(2, Time.valueOf(duracao)); //exemplo de hora
+            ps.setDate(3, Date.valueOf(dataEstreia)); //exemplo de data
+            ps.setString(4, genero); //Exemplo de String
             ps.execute(); //executa SQL preparada
             return true;
         } catch (SQLException | ClassNotFoundException ex) {
-            Logger.getLogger(PessoaDao.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(FilmeDao.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
     }
 
     public static void main(String[] args) {
-        //crie um objeto da classe Cliente controller
-        PessoaDao dao = new PessoaDao();
+        //crie um objeto da classe 
+        FilmeDao dao = new FilmeDao();
         //chame o método inserir desse objeto
-        boolean result = dao.inserir("Pietra", 01030371016);
+        boolean result = dao.inserir("Poeria em alto mar", "02:33:00", "1953-02-15", "drama");
         if (result) {
             JOptionPane.showMessageDialog(null, "Inserção realizada com sucesso!");
         } else {
             JOptionPane.showMessageDialog(null, "Problemas com a inserção!");
         }
-
-        //execute e confira no BD -- BD é muito legal, neh :)
     }
-
 }
